981cc2483335ff64951579748293526dcb354dfe
PixelGoogs <gulliverstrash@gmail.com>
2021-10-22 13:46:03 +0200

    New repository

---
+++ src/main/java/com/example/unitconversionapi/UnitConversionApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.unitconversionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitConversionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitConversionApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/unitconversionapi/UnitConversionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unitconversionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitConversionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
