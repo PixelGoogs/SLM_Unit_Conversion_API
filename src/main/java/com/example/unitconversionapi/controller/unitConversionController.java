2a3ccd9d6f7f910d9e0607de7a1ca4ec89b89727
PixelGoogs <gulliver89@gmail.com>
2022-02-04 11:53:07 +0100

    created controllers

---
+++ src/main/java/com/example/unitconversionapi/controller/unitConversionController.java
@@ -0,0 +1,14 @@
+package com.example.unitconversionapi.controller;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@SpringBootApplication
+//@GetMapping("/api")
+public class unitConversionController {
+
+    //@Getmapping("/convert")
+    //public
+}
