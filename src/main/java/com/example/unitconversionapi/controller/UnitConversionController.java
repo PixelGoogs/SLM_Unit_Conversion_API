d116f6d03247151f4cac867ef83f3ba3375d47b7
OreoCassowary <arthurkaplan@gmx.at>
2022-02-18 11:24:24 +0100

    Switched from @Controller to @RestController to simplify Controller * Also changed GetMapping to RequestMapping to cover all types of HTTP Requests * Renamed unitConversionController to UnitConversionController (following Java class naming convention)

--- src/main/java/com/example/unitconversionapi/controller/UnitConversionController.java
+++ src/main/java/com/example/unitconversionapi/controller/UnitConversionController.java
@@ -4,10 +4,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-@SpringBootApplication
-//@GetMapping("/api")
-public class unitConversionController {
+@RestController
+@RequestMapping("api")
+public class UnitConversionController {
 
     //@Getmapping("/convert")
     //public
