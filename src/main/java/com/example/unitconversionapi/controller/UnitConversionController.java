50785f6b7d2909da83c1877d6b464e323ac8e739
OreoCassowary <arthurkaplan@gmx.at>
2022-02-18 11:24:53 +0100

    Added imports to UnitConversionController

--- src/main/java/com/example/unitconversionapi/controller/UnitConversionController.java
+++ src/main/java/com/example/unitconversionapi/controller/UnitConversionController.java
@@ -1,8 +1,9 @@
 package com.example.unitconversionapi.controller;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
