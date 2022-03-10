b6a2d43dbb8e874dd130389fc0046d7071f96a74
OreoCassowary <arthurkaplan@gmx.at>
2022-03-10 13:34:53 +0100

    Renamed test package

--- src/test/java/com/example/unitconversionapi/strategiesTest/CToFStrategyTest.java
+++ src/test/java/com/example/unitconversionapi/strategiesTest/CToFStrategyTest.java
@@ -1,4 +1,4 @@
-package com.example.unitconversionapi.assertionTest;
+package com.example.unitconversionapi.strategiesTest;
 
 import com.example.unitconversionapi.strategies.CToFStrategy;
 import org.junit.jupiter.api.Test;
