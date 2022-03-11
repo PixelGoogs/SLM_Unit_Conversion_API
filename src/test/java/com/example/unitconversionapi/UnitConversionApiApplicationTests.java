06853f99d5a60799863d977f46d4815242e1fefd
OreoCassowary <arthurkaplan@gmx.at>
2022-03-11 11:28:34 +0100

    added integration test expecting bad request if no request body

--- src/test/java/com/example/unitconversionapi/UnitConversionApiApplicationTests.java
+++ src/test/java/com/example/unitconversionapi/UnitConversionApiApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.unitconversionapi;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class UnitConversionApiApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void noBodyShouldReturnBadRequest() throws Exception {
+        this.mockMvc.perform(post("/api/convert")).andExpect(status().isBadRequest());
     }
 
 }
