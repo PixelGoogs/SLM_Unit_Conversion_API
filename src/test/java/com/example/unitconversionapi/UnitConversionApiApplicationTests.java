c2e2c7b451beb0b96f3f80c46592cf40ba8d3cbe
OreoCassowary <arthurkaplan@gmx.at>
2022-03-11 14:32:45 +0100

    Added integration test expecting Status.OK if sending valid request

--- src/test/java/com/example/unitconversionapi/UnitConversionApiApplicationTests.java
+++ src/test/java/com/example/unitconversionapi/UnitConversionApiApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -23,6 +24,14 @@ class UnitConversionApiApplicationTests {
     @Test
     public void noBodyShouldReturnBadRequest() throws Exception {
         this.mockMvc.perform(post("/api/convert")).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void sendPostRequestWithBody_expect200() throws Exception {
+        this.mockMvc.perform(post("/api/convert")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"conversionUnits\": \"kgToG\", \"inputValue\": 10}"))
+                .andExpect(status().isOk());
     }
 
 }
