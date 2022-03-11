08ffaaf1de12f4e7ac0e2613b53afade3b4f97ba
OreoCassowary <arthurkaplan@gmx.at>
2022-03-11 09:59:48 +0100

    Response: changed "fromValue" to "inputValue"

--- src/main/java/com/example/unitconversionapi/dto/Response.java
+++ src/main/java/com/example/unitconversionapi/dto/Response.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Response {
-    private double fromValue;
+    private double inputValue;
     private String conversionUnits;
     private double result;
 
